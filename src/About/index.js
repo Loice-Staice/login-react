07acffbc8fef6071517a812a5879ff73849fe1cc
Loice-Staice <staiceloicea@gmail.com>
2024-06-21 19:17:36 +0300

    login files

---
+++ src/About/index.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+const About = () => {
+    return(
+        <div className="about-text">
+            <p>We are learning about JSX</p>
+            <p>We are coding in a new Frond-End library</p>
+        </div>
+    )
+}
+export default About;
---
+++ src/App.js
@@ -0,0 +1,20 @@
+ import Login from "./Login";
+ import Users from "./User";
+ 
+ export const Introduction =() =>{
+  return(
+    <h2>What Is ReactJs</h2>
+  );
+  };
+
+const App=()=>{
+  return(
+    <div>
+      {/* <Login/> */}
+      <Users/>
+      
+    </div>
+  )
+
+}
+export default App;
